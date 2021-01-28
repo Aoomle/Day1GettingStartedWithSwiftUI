5e53195eadc81eb0d43453ef1f560edb18fb7938
Abdulmalik Muhammad <bawasuru@gmail.com>
2021-01-28 10:06:31 +0100

    Initial Commit

---
+++ Day1GettingStartedWithSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day1GettingStartedWithSwiftUI
+//
+//  Created by Abdulmalik Muhammad on 28/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
